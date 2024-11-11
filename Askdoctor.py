d182266e0ebadddc6c1739470781dcee94287617
kannan-sankara <156016583+kannan-sankara@users.noreply.github.com>
2024-11-11 10:36:44 -0500

    Update Askdoctor.py

--- Askdoctor.py
+++ Askdoctor.py
@@ -22,7 +22,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key="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"), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
 
 
